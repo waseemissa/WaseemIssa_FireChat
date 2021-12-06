4c91a46b8ac6e18a79b4997202aefa14de46ea99
Waseem Issa <waseem.issa88@gmail.com>
2021-12-06 14:25:31 +0200

    Updates

---
+++ firebase-sw.js
@@ -0,0 +1,13 @@
+importScripts('https://www.gstatic.com/firebasejs/8.2.0/firebase-app.js');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBDdpNGm-Z1fWvd1UZhhsXUX58-MbE7VqI",
+    authDomain: "fir-chat-eaae3.firebaseapp.com",
+    projectId: "fir-chat-eaae3",
+    storageBucket: "fir-chat-eaae3.appspot.com",
+    messagingSenderId: "119782918399",
+    appId: "1:119782918399:web:1b3805c4e889d0341764ca",
+    measurementId: "G-51CLJ89K0Z"
+  };
+
+firebase.initializeApp(firebaseConfig);
